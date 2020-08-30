93dcb7ae073aefada0b4db9596af1134b9709aef
ederfmatos <ederfmatos@gmail.com>
2020-08-30 19:08:00 -0300

    FIX: Corrigido estrutura da tabela de pokemons

--- backend/src/main/resources/db/changelog/migrations/001_schema_inicial.sql
+++ backend/src/main/resources/db/changelog/migrations/001_schema_inicial.sql
@@ -1,18 +1,12 @@
 --liquibase formatted sql
 
---changeset david:1
+--changeset David Chaves:1
 CREATE TABLE IF NOT EXISTS pokemon (
     id VARCHAR(255) NOT NULL CONSTRAINT pokemon_pk PRIMARY KEY,
+    name VARCHAR(255) NOT NULL CONSTRAINT name_uk UNIQUE,
     number BIGINT NOT NULL,
-    name VARCHAR(255) CONSTRAINT name_uk UNIQUE
+    primaryType VARCHAR(50) NOT NULL,
+    secondaryType VARCHAR(50) NOT NULL
 );
 
 --rollback DROP TABLE pokemon
---rollback DROP SEQUENCE hibernate_sequence;
-
-CREATE TABLE IF NOT EXISTS types (
-    type VARCHAR(255),
-    pokemon_id VARCHAR(255) CONSTRAINT types_pokemon_fk REFERENCES pokemon
-);
---rollback ALTER TABLE types DROP CONSTRAINT types_pokemon_fk;
---rollback DROP TABLE types;
