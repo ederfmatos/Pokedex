41134ee30b49b01c9a736020148153803c6b1435
ederfmatos <ederfmatos@gmail.com>
2020-09-30 13:12:23 -0300

    Removido teste

--- backend/src/test/java/com/whatfucksoftware/pokedex/PokedexApplicationTests.java
+++ backend/src/test/java/com/whatfucksoftware/pokedex/PokedexApplicationTests.java
@@ -3,11 +3,6 @@ package com.whatfucksoftware.pokedex;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PokedexApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
