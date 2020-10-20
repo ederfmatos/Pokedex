ce5c6af01c618ff7a03e3e9fea1a88a84bbc53ed
Allan <allanhsz@outlook.com>
2020-10-19 21:40:09 -0300

    REFACTOR: migrando logica da lista de pokemons para a home

--- web/src/components/Pokemon/PokemonList.js
+++ web/src/components/Pokemon/PokemonList.js
@@ -1,36 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PokemonItem from './PokemonItem';
-import PokemonService from '../../services/PokemonService';
-import Spinner from '../Spinner';
-import Error from '../Error';
 
-const PokemonList = () => {
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState({});
-  const [pokemons, setPokemons] = useState([]);
-
-  useEffect(() => {
-    setLoading(true);
-    PokemonService.findAll()
-      .then(response => {
-        setError(null);
-        setPokemons(response);
-      })
-      .catch(setError)
-      .finally(() => setLoading(false));
-  }, []);
-
-  if (loading) {
-    return (
-      <div className="align-center homePageContent">
-        <Spinner />
-      </div>
-    );
-  }
-
-  if (error)
-    return <Error error={error} className="align-center homePageContent" />;
-
+const PokemonList = ({ pokemons }) => {
   return (
     <div className="pokemonList">
       {pokemons.map(pokemon => {
--- web/src/pages/Home/index.js
+++ web/src/pages/Home/index.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import { PokemonList, Header } from '../../components';
+import React, { useState, useEffect } from 'react';
+import { PokemonList, Header, Spinner, Error } from '../../components';
+import PokemonService from '../../services/PokemonService';
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState({});
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    PokemonService.findAll()
+      .then(response => {
+        setError(null);
+        setPokemons(response);
+      })
+      .catch(setError)
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <>
       <Header />
-      <main className="homePage">
-        <PokemonList />
-      </main>
+      {loading ? (
+        <div className="align-center fullScreen">
+          <Spinner />
+        </div>
+      ) : error ? (
+        <Error
+          error={error}
+          className="align-center 
+        "
+        />
+      ) : (
+        <main className="homePage">
+          <PokemonList pokemons={pokemons} />
+        </main>
+      )}
     </>
   );
 };
